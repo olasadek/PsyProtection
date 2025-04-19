b622ccc1d7d414f5bc5f44a38919903e1f71af9c
Ola sadek <158104182+olasadek@users.noreply.github.com>
2025-04-19 03:10:06 +0300

    Delete chatbot_router.py directory

--- chatbot_router.py/generate-query.py
+++
@@ -1,8 +0,0 @@
-class QueryInput(BaseModel):
-    medicine: str
-    illness: str
-
-@app.post("/generate-query/")
-def generate_query(input: QueryInput):
-    query = f"What are the best measurements for {input.medicine} addiction in {input.illness} patients?"
-    return {"query": query}
